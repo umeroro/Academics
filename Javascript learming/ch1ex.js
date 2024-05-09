b29180d2978ca1366968e9c4fec28847def6b93f
umeroro <161680343+umeroro@users.noreply.github.com>
2024-05-09 19:20:04 +0500

    Practice Programs ch1

---
+++ Javascript learming/ch1ex.js
@@ -0,0 +1,26 @@
+// create a variable of type string and try to add a number to it
+let a = 'umeroro';
+let b = 786;
+console.log(a+b)
+// print the type of the variable of last question
+console.log(typeof(a+b))
+// create a const object in js can you change it to hold a number later?
+const student = {
+  namee : "umeroro",
+  age : 56
+}
+// student = 1 (we cannot hold the number in the object)
+
+//try to add a new key to the const object in problem 3 were you able to do it?
+student['section'] = "A"
+console.log(student)
+// write a js program to create a word-meaning dictionary of 5 words
+
+const dist = {
+  book1 : "rich dad and poor dad",
+  book2 : "abc",
+  book3 : "def",
+  book4 : "ghi",
+  book5 : "ijk"
+}
+console.log(dist.book1)
