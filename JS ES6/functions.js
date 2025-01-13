cc8b569090e652d3ef41a6fc54e9433bc0f12f46
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-01-13 23:30:21 +0500

    also High Order and callback function

--- JS ES6/functions.js
+++ JS ES6/functions.js
@@ -8,3 +8,13 @@ function addNum(...numbers){
 
 let result = addNum(1,2,3,4,5,6);
 console.log(result);
+
+function mulNum(a,b,cb){
+    let ans = 0;
+    ans = a*b;
+    cb(ans)
+}
+
+mulNum(2,3,(val)=>{
+    console.log(val);
+})
