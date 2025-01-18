960e8d3f659b9d60689413949d99f22ce85441c8
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-01-18 19:12:29 +0500

    Arrays in js

---
+++ JS ES6/arrays.js
@@ -0,0 +1,9 @@
+const team = ["U","G","H"];
+team.push("M","N","o");
+//heterogenous array in js
+const details = [12,'Sins',100,"A",true,"😁"];
+details.push({semester: '7th'})
+
+console.log(details.indexOf(true));
+console.log(details);
+console.log(team)
