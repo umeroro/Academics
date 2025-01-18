9a790e87a995affaceb59b34964a4da8f2e10b3b
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-01-18 21:33:09 +0500

    Adding Higher Order function in array

--- JS ES6/arrays.js
+++ JS ES6/arrays.js
@@ -7,3 +7,34 @@ details.push({semester: '7th'})
 console.log(details.indexOf(true));
 console.log(details);
 console.log(team)
+
+const Names = ["Lahore","Multan","Peshwar","Islamabad","Queta","Karachi"]
+//forEach function
+let newName = Names.forEach((val)=>{console.log(val)});
+
+
+
+const number = [1,2,3,4,5]
+
+//Map Function
+let mapFunction = number.map((n)=>{
+    return n*2;
+})
+console.log(mapFunction)
+//find
+let findFunction = number.find((val)=>{ return val === 5})
+console.log(findFunction)
+//indexof
+let indexFunction = number.indexOf((val)=>{ return val === 2})
+console.log(indexFunction)
+//filter
+let filterFunction = number.filter((val)=>{ return val % 2 === 0})
+console.log(filterFunction)
+//slice
+let sliceFunction = number.slice(2);
+console.log(sliceFunction)
+//splice
+let spliceFunction = number.splice(1,2);
+console.log(sliceFunction)
+
+console.log(number);
