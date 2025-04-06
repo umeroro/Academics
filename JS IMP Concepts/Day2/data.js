95506b9b0cfc7699764a6f0488ef1cc13ac14507
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-04-06 22:08:44 +0500

    Day 2 practice task

---
+++ JS IMP Concepts/Day2/data.js
@@ -0,0 +1,9 @@
+const students = [
+    { name: "Ali", age: 20, progress: 15 },
+    { name: "Sara", age: 18, progress: 25 },
+    { name: "Zain", age: 22, progress: 10 }
+];
+
+const dataFunc = ()=> students;
+
+module.exports = {dataFunc};
