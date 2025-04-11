4c898c0852238c9fe620b21fe089b9e113bdc78e
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-04-11 12:17:39 -0700

    Mongo DB connect practice

--- JS IMP Concepts/db.js
+++ JS IMP Concepts/db.js
@@ -1,9 +1,15 @@
-const {MongoClient} = require('mongoo');
+const {MongoClient} = require('mongodb');
 const uri = 'mongodb+srv://barterweb:barterweb@cluster0.ukaok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const client = new MongoClient(uri)
+const client = new MongoClient(uri);
 
-async function connectMongo() {
+async function mongoConnect(){
     try {
         await client.connect();
+        console.log('Database is Connected');
+        return client.db('mern db');
+    } catch (error) {
+        return console.log(error);
     }
 }
+
+mongoConnect();
