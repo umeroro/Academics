bd30f38ea0c25e4b70cde444a50ae7789b1c6505
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-04-11 11:49:36 -0700

    Mongo DB learning

--- JS IMP Concepts/db.js
+++ JS IMP Concepts/db.js
@@ -1,3 +1,9 @@
 const {MongoClient} = require('mongoo');
 const uri = 'mongodb+srv://barterweb:barterweb@cluster0.ukaok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(uri)
+
+async function connectMongo() {
+    try {
+        await client.connect();
+    }
+}
