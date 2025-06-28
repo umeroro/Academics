3ccce618ca0f4b38ad338f659b73f6f79c933199
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-06-28 20:06:15 +0500

    Conversion Operations

---
+++ JS ES6/Thapa js/conversionOperations.js
@@ -0,0 +1,13 @@
+let score = 100;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let stringScore = String(score);
+console.log(typeof stringScore);
+
+let isLoggedIn = true;
+console.log(typeof isLoggedIn);
+
+let ValueLoggedIn = Number(isLoggedIn);
+console.log(typeof ValueLoggedIn);
