e0304a71abaff3543e5a82b68d8f60326925600e
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-06-28 20:06:03 +0500

    DataTypes in JS

---
+++ JS ES6/Thapa js/dataType.js
@@ -0,0 +1,10 @@
+let name = "jerry";
+let age = 20;
+let boolean = true;
+let state;
+let progress = null;
+
+console.table([name, age, boolean, state, progress]);
+
+console.log(typeof null);
+console.log(typeof undefined);
