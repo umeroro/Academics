1c7ca348f9ffe4da22d2cf7770438bdf916e0f89
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-06-28 20:05:53 +0500

    Variables and their scope in JS

---
+++ JS ES6/Thapa js/variables.js
@@ -0,0 +1,15 @@
+const accountId = 123;
+let accountName = "ummorro";
+var accountEmail = "xyz@gmail.com";
+accountCity = "faisalabad";
+let accountState;
+
+// accountId = 222 ;
+
+console.log(accountId);
+
+accountName = 'jerry';
+accountEmail = "test@gmail.com";
+accountCity = "pindi";
+
+console.table([accountName, accountEmail, accountCity, accountState]);
