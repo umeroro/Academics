bd7752088671b63be9fd6090a3173d588941cc57
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-06-29 22:51:47 +0500

    datatypes-summary

---
+++ JS ES6/Thapa js/dataTypes-summary.js
@@ -0,0 +1,19 @@
+// primitive data types
+
+// number , string , Boolean , null , undefined , symbol , biginit
+
+const number = 123;
+const string = letter;
+const isLoggedIn = false;
+const state = null;
+let city;
+
+// non-primitive data types
+
+// array , objects
+
+const fruits = ["mango", "cherry", "watermelon"];
+const information = {
+  name: "xyz",
+  age: "01",
+};
