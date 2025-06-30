958029a0d2f066eaf9641d284869454d205cb66a
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-06-30 23:27:21 +0500

    Stack & Heap Learning

--- JS ES6/Thapa js/dataTypes-summary.js
+++ JS ES6/Thapa js/dataTypes-summary.js
@@ -3,7 +3,7 @@
 // number , string , Boolean , null , undefined , symbol , biginit
 
 const number = 123;
-const string = letter;
+const string = "letter";
 const isLoggedIn = false;
 const state = null;
 let city;
@@ -17,3 +17,26 @@ const information = {
   name: "xyz",
   age: "01",
 };
+
+//stack (primitive) and heap (non-primitive)
+
+//stack provide copy not orginal
+
+let fruitNameOne = "cherry";
+let fruitNametwo = fruitNameOne;
+fruitNametwo = 'mango'
+console.log(fruitNameOne);
+console.log(fruitNametwo);
+
+//heap
+
+let userOne = {
+  email: "userOne@gmail.com",
+  age: 30,
+};
+
+let userTwo = userOne;
+userTwo.email = 'userTwo@gmail.com'
+
+console.log(userOne.email)
+console.log(userTwo.email)
