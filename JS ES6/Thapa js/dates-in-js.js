a62c902d1c384a9b364e41bafb183637c9d3a8c5
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-07-03 23:45:39 +0500

    Dates in JS

---
+++ JS ES6/Thapa js/dates-in-js.js
@@ -0,0 +1,12 @@
+let newDate = new Date();
+
+console.log(newDate);
+console.log(newDate.toString());
+console.log(newDate.toISOString());
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
+console.log(newDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2023, 6, 3, 11, 40);
+let myCreatedDate = new Date("06-03-2023");
+console.log(myCreatedDate.toLocaleString());
