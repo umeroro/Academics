d2e1af2eacf7ef6c9e07ff22d37de18bc503c558
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-07-07 23:31:38 +0500

    Array

--- JS ES6/Thapa js/arrays.js
+++ JS ES6/Thapa js/arrays.js
@@ -1,9 +1,14 @@
-const myArr = [0, 1, 2, 3, 4]
+const myArr = [0, 1, 2, 3, 4];
 
 console.log(myArr.push(5));
 console.log(myArr);
-console.log(myArr.pop(1));
+console.log(myArr.pop());
+console.log(myArr.unshift(9));
+console.log(myArr);
+console.log(myArr.slice(0,2));
+console.log(myArr);
+console.log(myArr.splice(0,2));
 console.log(myArr);
 
-const myArr2 = new Array([1,2,4,5,6])
+const myArr2 = new Array([1, 2, 4, 5, 6]);
 console.log(myArr2);
--- JS ES6/Thapa js/tempCodeRunnerFile.js
+++ JS ES6/Thapa js/tempCodeRunnerFile.js
@@ -1,1 +1,2 @@
-06-03-2023
+ew Array([1, 2, 4, 5, 6]);
+// console.log(myArr2);
