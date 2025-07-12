6ee4789a0f94c0b501b44f68fc6553bf00f16d3e
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-07-12 22:48:39 +0500

    Array Practices

---
+++ JS ES6/Thapa js/array-practices.js
@@ -0,0 +1,9 @@
+let array = [1,2,3,4,5]
+
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+}
+
+console.log(array.push(6));
+let newArray = array.pop();
+console.log(newArray);
